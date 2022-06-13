504f221b8584e15bd5686b75b0f7d291ec6885d4
Crizien Navarro <navarro.c.bscs@gmail.com>
2022-06-13 22:26:06 +0800

    ...

---
+++ app/src/main/java/com/example/luminousbank/Home.java
@@ -0,0 +1,27 @@
+package com.example.luminousbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_home);
+    }
+
+    //Call Login Screen
+    public void callLoginScreen(View view){
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/luminousbank/Splash.java
@@ -0,0 +1,25 @@
+package com.example.luminousbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),Home.class));
+                finish();
+            }
+        },2000);
+    }
+}
