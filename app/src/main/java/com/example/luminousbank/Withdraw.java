56cc7238742e3dbd48be054848559e36470c32db
Christine <christinemaecion@gmail.com>
2022-07-15 14:49:34 +0800

    Added Savings (Deposit&Withdraw)

--- app/src/main/java/com/example/luminousbank/Withdraw.java
+++ app/src/main/java/com/example/luminousbank/Withdraw.java
@@ -5,17 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Withdraw extends AppCompatActivity {
+
+    EditText withdrawamount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_withdraw);
+
+        withdrawamount = findViewById(R.id.withdraw_amount);
+
     }
 
     public void callWithdrawBackBtn(View view) {
         Intent intent = new Intent(Withdraw.this, Savings.class);
+        startActivity(intent);
+    }
+
+    //VALIDATE AMOUNT
+    private Boolean validateAmount() {
+        String val = withdrawamount.getText().toString();
+        if (val.isEmpty()) {
+            withdrawamount.setError("Field cannot be empty");
+            return false;
+        } else {
+            withdrawamount.setError(null);
+            return true;
+        }
+    }
+
+    public void callWithdrawDashboard(View view) {
+
+        if (!validateAmount()) {
+            return;
+        }
+
+        Intent intent = new Intent(getApplicationContext(), Dashboard.class);
         startActivity(intent);
     }
 }
